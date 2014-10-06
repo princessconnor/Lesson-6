115de9f7c261f0cc753d0debaf784b6b49a6ceb0
conn6070 <conn6070@ISS147D8-03.scdsb.pri>
2014-10-06 09:13:39 -0400

    started guessing game (oct 6th)

---
+++ src/GuessMachine.java
@@ -0,0 +1,17 @@
+
+public class GuessMachine {
+   private int number;
+   private int numguesses; 
+   private int guess;
+   
+   public GuessMachine(){
+    number =(int)(Math.random()*100)+1;
+    guess=0;
+    numguesses = 0;
+    //to do - add methods
+    //givehints(), setGuess(), getNumGuesses()
+       
+       
+       
+   }
+}
