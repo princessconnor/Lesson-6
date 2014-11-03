08770dc4377db96877d3f6220f6e1ec5a8e25bce
conn6070 <conn6070@ISS147D8-03.scdsb.pri>
2014-11-03 09:19:55 -0500

    Lesson 8git

--- src/MakeEmployees.java
+++ src/MakeEmployees.java
@@ -11,16 +11,44 @@ import TerminalIO.KeyboardReader;
  *
  * @author conn6070
  */
+import TerminalIO.*;
 public class MakeEmployees {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-  KeyboardReader kr = new KeyboardReader();
-  String name = kr.readLine("Enter Name-> ");
-        System.out.println("the name is: " + name);
-        System.out.println("this is another line doggy");
-    }
-    
+public static void main(String[] args) { 
+    KeyboardReader k=new KeyboardReader();
+ Employee emp; 
+ String name; 
+ int type; 
+ double rate; 
+ int hours; 
+ String prompt; 
+ emp=new Employee(); 
+ while (true){ //get the name and break if blank
+     System.out.println("Enter employee data");
+     name=k.readLine(" Name (or <enter> to quit): ");
+     if(!(emp.setName(name)))break; 
+//get the type until valid
+     while(true) { 
+         prompt=" Type ("+emp.getTypeRules()+"): ";
+      type=k.readInt(prompt);
+      if (emp.setType(type))break;
+     }
+     //Get hourly pay rate until valid 
+     while(true){
+         prompt= "Hourly rate("+emp.getRateRules()+"): ";
+         rate=k.readDouble(prompt);
+         if(emp.setRate(rate))break; 
+     }
+     //get the hours worked until valid 
+     while(true){ prompt=" Hours("+emp.getHourRules()+"): ";
+     hours=k.readInt(prompt);
+     }
+     if(emp.setHours(hours))break;
+ }
+ //print the name and pay
+ System.out.println(" The Weekly pay for "); 
+ System.out.println(emp.getName()); 
+ System.out.println(" is $"+emp.getPay()); 
+}//end larger while
+}//end main
 }
